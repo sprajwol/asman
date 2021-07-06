ab21d69bacc12058ce60e824cb2fbdba9145ef98
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-07-06 13:59:09 +0545

    topbar

--- home/views.py
+++ home/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
+# from home.models import HeroSlider
+# from about.models import Testimonial, Member
+# from projects.models import Project, ProjectCategory
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        # if (HeroSlider.objects.all().exists()):
+        #     hero_slider_data = HeroSlider.objects.all()
+        # else:
+        #     hero_slider_data = 'no_data'
+
+        # if (ProjectCategory.objects.all().exists()):
+        #     project_cat_data = ProjectCategory.objects.all()
+        # else:
+        #     project_cat_data = 'no_data'
+
+        # if (Project.objects.all().exists()):
+        #     project_data = Project.objects.all()
+        # else:
+        #     project_data = 'no_data'
+
+        # if (Member.objects.all().exists()):
+        #     member_data = Member.objects.all()[:3]
+        # else:
+        #     member_data = 'no_data'
+
+        # if (Testimonial.objects.all().exists()):
+        #     testimonial_data = Testimonial.objects.all()
+        # else:
+        #     testimonial_data = 'no_data'
+
+        context['home_page'] = 'active'
+        # context['hero_slider_data'] = hero_slider_data
+        # context['testimonial_data'] = testimonial_data
+        # context['member_data'] = member_data
+        # context['project_data'] = project_data
+        # context['project_cat_data'] = project_cat_data
+        return context
