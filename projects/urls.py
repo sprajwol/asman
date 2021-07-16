d8bf006bcd2d226147e2118339bb668d227bfbf4
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-07-16 23:27:16 +0545

    projects url fixes; views templates, css urls

--- projects/urls.py
+++ projects/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from contact import views as contact_views
+from projects import views as projects_views
 
 urlpatterns = [
-    path('', contact_views.ContactView.as_view(), name='contact')
+    path('', projects_views.ProjectsView.as_view(), name='projects')
 ]
 
 if settings.DEBUG:
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+
+class ProjectsView(TemplateView):
+    template_name = 'projects/projects.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        context['projects_page'] = 'active'
+        return context
